605e5c8919c31e6adde3ddbad3595132efd8bc32
unknown <ahmed.omar.alfarouq@gmail.com>
2021-06-02 08:17:43 +0200

    Add 404 Page

--- pages/404.jsx
+++ pages/404.jsx
@@ -1,6 +1,23 @@
+import Link from 'next/link';
+import Head from 'next/head';
+import { TiDelete } from 'react-icons/ti';
+import MainBanel from '../components/Common/MainBanel';
+
 const NotFound = () => {
     return (
-        <h1>Page Is Not Exist</h1>
+        <>
+            <Head>
+            <title>Burgizza  | Not Found</title>
+            </Head>
+            <MainBanel h1Text="Wrong Direction" pText="Maybe Link Is corrupt" bg_url="/not_found_main_banel.jpg"/>
+            <section className="not_found">
+                <TiDelete/>
+                <h1>Page Is Not Found</h1>
+                <Link href="/"> 
+                    <a className="home_btn">Home</a>
+                </Link>
+            </section>
+        </>
     );
 }
  
